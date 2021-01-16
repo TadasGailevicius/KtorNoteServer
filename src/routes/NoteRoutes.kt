2073a5a8a19eec17fc4682c1f38802b51c90ebb4
Tadas <tadasgailevicius@yahoo.com>
2021-01-17 00:41:38 +0200

    Add addNote route to NoteRoutes.kt

--- src/routes/NoteRoutes.kt
+++ src/routes/NoteRoutes.kt
@@ -1,15 +1,22 @@
 package com.example.routes
 
+import com.example.data.collections.Note
 import com.example.data.getNotesForUser
+import com.example.data.saveNote
 import io.ktor.application.call
 import io.ktor.auth.UserIdPrincipal
 import io.ktor.auth.authenticate
 import io.ktor.auth.principal
+import io.ktor.features.ContentTransformationException
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
 
 fun Route.noteRoutes() {
@@ -23,4 +30,23 @@ fun Route.noteRoutes() {
             }
         }
     }
+
+    route("/addNote") {
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+                } catch (e: ContentTransformationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+                if(saveNote(note)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
+            }
+        }
+    }
+
 }
